4507911a0271e82e2813476f034ac8bd343808b1
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-07-29 11:02:32 +0200

    Added emplty event handler

---
+++ src/Fibon.Api/Handlers/ValueCalculatedEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Fibon.Messages;
+
+namespace Fibon.Api.Handlers
+{
+    public class ValueCalculatedEventHandler : IEventHandler<ValueCalculatedEvent>
+    {
+        public Task HandleAsync(ValueCalculatedEvent @event)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
