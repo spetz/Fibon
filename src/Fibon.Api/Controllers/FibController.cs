88d0ccd4d7422fb860c84b99d7862cf7bd4e9cb7
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-07-29 10:40:45 +0200

    Fib controller

---
+++ src/Fibon.Api/Controllers/FibController.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Fibon.Messages;
+using Microsoft.AspNetCore.Mvc;
+using RawRabbit;
+
+namespace Fibon.Api.Controllers
+{
+    [Route("[controller]")]
+    public class FibController : Controller
+    {
+        private readonly IBusClient _client;
+        private readonly IRepository _repo;
+
+        public FibController(IBusClient client,
+            IRepository repo)
+        {
+            _client = client;
+            _repo = repo;
+        }
+
+        [HttpGet("{number}")]
+        public IActionResult Get(int number)
+        {
+            int? result = _repo.Get(number);
+            if (result.HasValue)
+            {
+                return Content(result.ToString());
+            }
+
+            return Content("Not ready...");
+        }
+
+        [HttpPost("{number}")]
+        public async Task<IActionResult> Post(int number)
+        {
+            int? result = _repo.Get(number);
+            if (!result.HasValue)
+            {
+                await _client.PublishAsync(
+                    new CalculateValueCommand
+                    {
+                        Number = number
+                    });
+            }
+
+            return Accepted($"fib/{number}", null);
+        }
+    }
+
+    public interface IRepository
+    {
+        int? Get(int number);
+    }
+}
