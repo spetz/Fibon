5521ebbb0bf67b54caa4fd42d20807c51842caca
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-07-29 10:31:49 +0200

    Communication layer

--- src/Fibon.Messages/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Fibon.Messages
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Fibon.Messages/IEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace Fibon.Messages
+{
+    public interface IEvent
+    {   
+    }
+
+    public class ValueCalculated : IEvent
+    {
+        public int Number { get; set; }
+        public int Result { get; set; }
+    }
+
+    public interface IEventHandler<in T>
+        where T : IEvent
+    {
+        Task HandleAsync(T @event);
+    }
+}
