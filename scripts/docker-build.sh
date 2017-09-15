4d096787c62f73b7e625834c367ac00d2e6dd108
Piotr Gankiewicz <spetz90@gmail.com>
2017-09-15 11:11:26 +0200

    Updated scripts and configs'

--- scripts/docker-build.sh
+++
@@ -1,2 +0,0 @@
-docker build -t fibon-api ./src/Fibon.Api --no-cache
-docker build -t fibon-service ./src/Fibon.Service --no-cache
--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -1,5 +1,36 @@
+#!/bin/bash
+DOCKER_ENV=''
+DOCKER_TAG=''
+
+case "$TRAVIS_BRANCH" in
+  "master")
+    DOCKER_ENV=production
+    DOCKER_TAG=latest
+    ;;
+  "develop")
+    DOCKER_ENV=development
+    DOCKER_TAG=dev
+    ;;    
+esac
+
+case "$BITBUCKET_BRANCH" in
+  "master")
+    DOCKER_ENV=production
+    DOCKER_TAG=latest
+    ;;
+  "develop")
+    DOCKER_ENV=development
+    DOCKER_TAG=dev
+    ;;    
+esac
+
 docker login $DOCKER_REGISTRY -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker tag fibon-api $DOCKER_USERNAME/fibon-api
-docker tag fibon-service $DOCKER_USERNAME/fibon-service
-docker push $DOCKER_USERNAME/fibon-api
-docker push $DOCKER_USERNAME/fibon-service
+
+docker build -f ./src/Fibon.Api/Dockerfile.$DOCKER_ENV -t fibon-api:$DOCKER_TAG --no-cache
+docker build -f ./src/Fibon.Service/Dockerfile.$DOCKER_ENV -t fibon-service:$DOCKER_TAG --no-cache
+
+docker tag fibon-api:$DOCKER_TAG $DOCKER_USERNAME/fibon-api:$DOCKER_TAG
+docker tag fibon-service:$DOCKER_TAG $DOCKER_USERNAME/fibon-service:$DOCKER_TAG
+
+docker push $DOCKER_USERNAME/fibon-api:$DOCKER_TAG
+docker push $DOCKER_USERNAME/fibon-service:$DOCKER_TAG
