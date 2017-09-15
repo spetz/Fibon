5c5680c62cd24755a5668e54c05d019175b6ba47
Piotr Gankiewicz <spetz90@gmail.com>
2017-09-15 11:29:26 +0200

    Updated docker build

--- scripts/docker-publish-travis.sh
+++ scripts/docker-publish-travis.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 DOCKER_ENV=''
 DOCKER_TAG=''
 
@@ -15,8 +14,8 @@ esac
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-docker build -f ./src/Fibon.Api/Dockerfile.$DOCKER_ENV -t fibon-api:$DOCKER_TAG --no-cache
-docker build -f ./src/Fibon.Service/Dockerfile.$DOCKER_ENV -t fibon-service:$DOCKER_TAG --no-cache
+docker build -f ./src/Fibon.Api/Dockerfile.$DOCKER_ENV -t fibon-api:$DOCKER_TAG ./src/Fibon.Api --no-cache
+docker build -f ./src/Fibon.Service/Dockerfile.$DOCKER_ENV -t fibon-service:$DOCKER_TAG ./src/Fibon.Service --no-cache
 
 docker tag fibon-api:$DOCKER_TAG $DOCKER_USERNAME/fibon-api:$DOCKER_TAG
 docker tag fibon-service:$DOCKER_TAG $DOCKER_USERNAME/fibon-service:$DOCKER_TAG
