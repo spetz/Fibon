3decb5d37c80357b4671456e22784bf8fba65432
Piotr Gankiewicz <spetz90@gmail.com>
2017-09-15 11:02:50 +0200

    Fixed pipelines

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -1,4 +1,4 @@
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker login $DOCKER_REGISTRY -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker tag fibon-api $DOCKER_USERNAME/fibon-api
 docker tag fibon-service $DOCKER_USERNAME/fibon-service
 docker push $DOCKER_USERNAME/fibon-api
