78313d5ab77f75025be24e8f26bedffed16ebd29
Piotr Gankiewicz <spetz90@gmail.com>
2017-09-15 11:23:08 +0200

    Separate build scripts

--- scripts/docker-publish-pipelines.sh
+++ scripts/docker-publish-pipelines.sh
@@ -2,17 +2,6 @@
 DOCKER_ENV=''
 DOCKER_TAG=''
 
-case "$TRAVIS_BRANCH" in
-  "master")
-    DOCKER_ENV=production
-    DOCKER_TAG=latest
-    ;;
-  "develop")
-    DOCKER_ENV=development
-    DOCKER_TAG=dev
-    ;;    
-esac
-
 case "$BITBUCKET_BRANCH" in
   "master")
     DOCKER_ENV=production
